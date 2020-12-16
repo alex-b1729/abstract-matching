a64a13132e15510ab69652317788d42c8ecb7aa8
Alexander Brefeld <alexander.brefeld@protonmail.com>
2020-12-16 10:08:39 -0500

    Complete pdf to txt abstract function

--- abstrprep/__init__.py
+++ abstrprep/__init__.py
@@ -65,57 +65,98 @@ def gen_dict_corpus(names, assignment_group_path='assignment_groups'):
     return dictionary, corpus
     
 
-def extract_abstract(os_type, paper_file_path='papers_to_assign'):
+def extract_abstract(os_type, pdf_dir='papers_to_assign'):
     """Converts pdfs to txt and saves abstracts"""
     # xpdf path depends on os
     if os_type=='darwin':
         # mac
         xpdf_path = os.path.join(os.getcwd(), 'xpdf-tools-mac-4.02/bin64/pdftotext')
         
-    # txt abstract path
-    abstr_txt_path = os.path.join(paper_file_path, 'abstract_txts')
+    # pdf directory full path
+    pdf_dir_path = os.path.join(os.getcwd(), pdf_dir)
+    # txt abstract full path
+    abstr_txt_dir_path = os.path.join(pdf_dir_path, 'abstract_txts')
     
-    # abstract already converted to txt
-    txt_names = []
-    with os.scandir(os.path.join(os.getcwd(), abstr_txt_path)) as it:
+    # check if pdf path exists
+    if os.path.isdir(pdf_dir_path):
+        if not os.path.isdir(abstr_txt_dir_path):
+            # make abstract txt directory in pdf directory
+            os.mkdir(abstr_txt_dir_path)
+    else:
+        # somethings wrong with the paths
+        print('Directory not found: {}'.format(pdf_dir_path))
+
+    # pdfs names
+    pdf_file_names = []
+    with os.scandir(pdf_dir_path) as it:
+        for entry in it:
+            if entry.name.endswith('.pdf') and entry.is_file() and not entry.name.startswith('.'):
+                # save file name without extension
+                pdf_file_names.append(entry.name[:-4])
+                
+    # find abstracts already converted to txt
+    txt_file_names = []
+    with os.scandir(abstr_txt_dir_path) as it:
         for entry in it:
             if entry.name.endswith('.txt') and entry.is_file() and not entry.name.startswith('.'):
-                txt_names.append(entry.name)
-                
-    # list of paths to each .pdf not already converted to txt
-    submissions = []
-    sub_names = []
-    with os.scandir(os.path.join(os.getcwd(), paper_file_path)) as it:
-        for entry in it:
-            if entry.name.endswith('.pdf') and entry.is_file() and not entry.name.startswith('.') and entry.name not in txt_names:
-                submissions.append(entry.path)
-                sub_names.append(entry.name[:-4])
+                if entry.name[:-4] not in pdf_file_names:
+                    txt_file_names.append(entry.name)
                 
     # convert each pdf to txt using xpdf tool
     txt_issues = []
-    for paper_index in range(len(sub_names)):
-        txt_path = os.path.join(abstr_txt_path, '{}.txt'.format(sub_names[paper_index]))
-        paper_path = submissions[paper_index].path
-        comd = '{} -l 10 {} {}'.format(shlex.quote(xpdf_path), shlex.quote(paper_path), shlex.quote(txt_path))
-        # get text from .pdf
-        res = subprocess.run(comd, shell=True)
-        # if issue with conversion
-        if res.returncode!=0:
-            txt_issues.append(submissions[paper_index])
-        else:
-            # get text of abstract
-            for paper in sub_names:
-                if paper not in txt_issues:
-                    with open(paper.path, mode='r', errors='ignore') as file:
-                        paper_txt = file.read()
-                        abstr_txt = get_abstract(paper_txt)
+    abstr_issues = []
+    for paper_name in pdf_file_names:
+        if paper_name not in txt_file_names:
+            pdf_convert_path = os.path.join(pdf_dir_path, '{}.pdf'.format(paper_name))
+            txt_result_path = os.path.join(abstr_txt_dir_path, '{}.txt'.format(paper_name))
+            # convert first 10 pages
+            comd = '{} -l 10 {} {}'.format(shlex.quote(xpdf_path), shlex.quote(pdf_convert_path), shlex.quote(txt_result_path))
+            # get text from .pdf
+            res = subprocess.run(comd, shell=True)
+            # if issue with conversion
+            if res.returncode!=0:
+                txt_issues.append(paper_name)
+            else:
+                # get text of abstract
+                with open(txt_result_path, mode='r', errors='ignore') as file:
+                    abstr_txt = get_abstract(file.read())
+                    # overwrite txt file with abstract txt if abstract found
+                    if abstr_txt:
+                        file.seek(0)
+                        file.write(abstr_txt)
+                        file.truncate()
+                    else:
+                        abstr_issues.append(paper_name)
     
-    # # print txt file conversion issues
-    # print('Issues converting the following files to .txt:')
-    # for title in txt_issues:
-    #     print(title.name)
+    # if issues with conversion
+    if txt_issues != [] or abstr_issues != []:
+        # txt conversion issues
+        print('Issue converting following .pdf file to .txt:')
+        for paper in txt_issues:
+            print('{}.pdf'.format(paper))
+        # issues finding abstracts
+        print('Issues extracting abstract from following .txt files:')
+        for paper in abstr_issues:
+            print('{}.txt'.format(paper))
         
-    # extract abstracts
+        # wait to allow user to manually save txt abstracts
+        input('Please add missing abstracts to abstract_txts directory as a .txt file then press Enter...')
+            
+        # double check that all abstracts are present
+        all_converted = False
+        while not all_converted:
+            any_not_converted = False
+            with os.scandir(abstr_txt_dir_path) as it:
+                for entry in it:
+                    if entry.name.endswith('.txt') and entry.is_file() and not entry.name.startswith('.'):
+                        if entry.name[:-4] not in pdf_file_names:
+                            print('Abstract .txt file for {} not found.'.format(entry.name[:-4]))
+                            any_not_converted = True
+            if any_not_converted:
+                input('Not all .txt files found. \nPlease add missing abstracts then press Enter...')
+            else:
+                all_converted = True
+        
 
 
     
@@ -136,6 +177,7 @@ class CleanAbstracts():
          gensim.utils.simple_preprocess(text, deacc=True)
          if wrd not in stop_words]
 
+
 class IterCorpus():
     def __init__(self, txt_paths):
         self.txt_paths = txt_paths
@@ -148,12 +190,6 @@ class IterCorpus():
                 yield dictionary.doc2bow([ps.stem(wrd) for wrd in 
                                           gensim.utils.simple_preprocess(file.read(), deacc=True)
                                           if wrd not in stop_words])
-
-class FindAbstract():
-    def __init__(self, txt_paths):
-        self.txt_paths = txt_paths
-    
-    def __iter__(self):
 
 
 def get_abstract(txt):
@@ -275,7 +311,12 @@ def get_abstract(txt):
             abstr = '{} {}'.format(abstr,jels)
         else:
             abstr = jels
-            
+    
+    # issue if abstract is too long or short
+    abstr_wrds = len(abstr.split())
+    if abstr_wrds < 50 or abstr_wrds > 200:
+        abstr = None
+
     return abstr
 
 # =============================================================================
